bd5f45cf0a5bc65d7e634975514291c6aa5d9a57
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-05 11:05:43 +0000

    piet mondrian

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+const CELSIZES = [10, 20, 50];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 800);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  const CELLSIZE = 20;
+  noStroke();
+  ///////////////
+  //simple grid//
+  ///////////////
+  /*
+  for(let y = 0; y < height; y += CELLSIZE){
+    for(let x = 0; x < width; x += CELLSIZE) {
+      fill(random(255));
+      rect(x, y, CELLSIZE);
+    }
+  } */
+
+  /////////////////
+  //Piet Mondrian//
+  /////////////////
+
 }
