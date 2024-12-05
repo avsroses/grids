d88e129de6f402f9c8c3cd8fc2a2157df2678754
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-05 11:31:29 +0000

    add piet mondrian code

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
-const CELSIZES = [10, 20, 50];
+const CELLSIZES = [100, 160, 200, 400];
+const COLOURS = ["#FF0000", "#FFFF00", "#FFFFF0", "#00008B", "#FFFFF0", "#000000"];
 
 function setup() {
-  createCanvas(600, 800);
+  createCanvas(800, 800);
   noLoop();
 }
 
 function draw() {
   background(220);
 
-  const CELLSIZE = 20;
-  noStroke();
+  //noStroke();
   ///////////////
   //simple grid//
   ///////////////
   /*
-  for(let y = 0; y < height; y += CELLSIZE){
+  const CELLSIZE = 20;
+  for(let y = 0; y < height; y += CELLSIZE) {
     for(let x = 0; x < width; x += CELLSIZE) {
       fill(random(255));
       rect(x, y, CELLSIZE);
@@ -24,5 +25,35 @@ function draw() {
   /////////////////
   //Piet Mondrian//
   /////////////////
+  /*
+  let x = 0;
+  let y = 0;
+  let currentWidth = random(CELLSIZES);
+  let currentHeight = random(CELLSIZES);
+  strokeWeight(5);
+  while(y < height) {
+    while(x < width) {
+      fill(random(COLOURS));
+      rect(x, y, currentWidth, currentHeight);
+      x += currentWidth;
+      currentWidth = random(CELLSIZES);
+    }
+    x = 0;
+    y += currentHeight;
+    currentHeight = random(CELLSIZES);
+  } */
+
+  //////////////////
+  //grid of shapes//
+  //////////////////
+  
+  const CELLSIZE = 50;
+  for(let y = 0; y < height; y += CELLSIZE) {
+    for(let x = 0; x < width; x += CELLSIZE) {
+      fill(random(255));
+      rect(x, y, CELLSIZE);
+    }
+  } 
 
 }
+ 
